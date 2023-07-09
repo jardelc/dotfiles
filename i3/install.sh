aefbb831bfb1470ce4d40677a3dee8196240a787
Jardel Leonardi <jardelc.leonardi@gmail.com>
2023-07-08 21:00:59 -0400

    Sound and bluetooth added, picom adjusted

--- i3/install.sh
+++ i3/install.sh
@@ -35,7 +35,7 @@ install() {
 
 # Main
 main() {
-	install_dependencies "picom nitrogen polybar dunst network-manager-applet alacritty gnome-screenshot archlinux-wallpaper"
+	install_dependencies "picom nitrogen polybar dunst network-manager-applet alacritty gnome-screenshot archlinux-wallpaper blueman pasystray"
 	install "I3" "config" "/etc/i3"
 }
 
